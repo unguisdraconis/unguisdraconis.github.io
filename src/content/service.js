96f2e3b44686dcccdd5ad6b4032b0b9ccd9aff00
Jeremiah King <jeremiah@vols.utk.edu>
2026-09-11 06:08:56 -0400

    updated wording about poster presentation.

--- src/content/service.js
+++ src/content/service.js
@@ -1,17 +1,19 @@
 export const presentations = [
   {
-    type: 'Listed by ALA as sole poster author and presenter',
+    type: "Authored and presented poster",
     title:
-      '1000 Libros: Small Libraries Serving Multilingual and Non-English-Speaking Populations (Poster 30)',
-    venue: 'ALA Annual Conference · Diversity Fair Poster Session · Philadelphia',
-    date: 'June 29, 2025',
+      "1000 Libros: Small Libraries Serving Multilingual and Non-English-Speaking Populations (Poster 30)",
+    venue:
+      "ALA Annual Conference · Diversity Fair Poster Session · Philadelphia",
+    date: "June 29, 2025",
   },
-]
+];
 
 export const service = [
   {
-    organization: 'American Library Association International Relations Round Table',
-    role: 'Webinar Committee volunteer',
-    period: '2025–2027 · Current',
+    organization:
+      "American Library Association International Relations Round Table",
+    role: "Webinar Committee volunteer",
+    period: "2025–2027 · Current",
   },
-]
+];
